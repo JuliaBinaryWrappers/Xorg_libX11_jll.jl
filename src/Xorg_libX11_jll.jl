8290c51148cc13ee0c095fde6162cb08456fdfe6
Elliot Saba <staticfloat@gmail.com>
2019-10-14 18:11:06 -0400

    Xorg_libX11_jll build 1.6.8+0

---
+++ src/Xorg_libX11_jll.jl
@@ -0,0 +1,31 @@
+module Xorg_libX11_jll
+using Pkg, Pkg.BinaryPlatforms, Pkg.Artifacts, Libdl
+import Base: UUID
+
+# We put these inter-JLL-package API values here so that they are always defined, even if there
+# is no underlying wrapper held within this JLL package.
+const PATH_list = String[]
+const LIBPATH_list = String[]
+
+# Load Artifacts.toml file
+artifacts_toml = joinpath(@__DIR__, "..", "Artifacts.toml")
+
+# Extract all platforms
+artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("4f6342f7-b3d2-589e-9d20-edeb45f2b2bc"))
+platforms = [Pkg.Artifacts.unpack_platform(e, "Xorg_libX11", artifacts_toml) for e in artifacts["Xorg_libX11"]]
+
+# Filter platforms based on what wrappers we've generated on-disk
+platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+
+# From the available options, choose the best platform
+best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
+
+# Silently fail if there's no binaries for this platform
+if best_platform === nothing
+    @debug("Unable to load Xorg_libX11; unsupported platform $(triplet(platform_key_abi()))")
+else
+    # Load the appropriate wrapper
+    include(joinpath(@__DIR__, "wrappers", "$(best_platform).jl"))
+end
+
+end  # module Xorg_libX11_jll
