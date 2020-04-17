70597bb442e7ee846662597955c59e58578b99cc
jlbuild <juliabuildbot@gmail.com>
2020-04-17 19:03:10 +0000

    Xorg_libX11_jll build 1.6.9+2

--- src/Xorg_libX11_jll.jl
+++ src/Xorg_libX11_jll.jl
@@ -11,7 +11,7 @@ if VERSION < v"1.3.0-rc4"
     # if they are willing to engage in the kinds of hoop-jumping they might need
     # to in order to install binaries in a JLL-compatible way on 1.0-1.2. One
     # example of this hoop-jumping being to express a dependency on this JLL
-    # package, then import it wtihin a `VERSION >= v"1.3"` conditional, and use
+    # package, then import it within a `VERSION >= v"1.3"` conditional, and use
     # the deprecated `build.jl` mechanism to download the binaries through e.g.
     # `BinaryProvider.jl`.  This should work well for the simplest packages, and
     # require greater and greater heroics for more and more complex packages.
@@ -34,7 +34,7 @@ artifacts = Pkg.Artifacts.load_artifacts_toml(artifacts_toml; pkg_uuid=UUID("4f6
 platforms = [Pkg.Artifacts.unpack_platform(e, "Xorg_libX11", artifacts_toml) for e in artifacts["Xorg_libX11"]]
 
 # Filter platforms based on what wrappers we've generated on-disk
-platforms = filter(p -> isfile(joinpath(@__DIR__, "wrappers", triplet(p) * ".jl")), platforms)
+filter!(p -> isfile(joinpath(@__DIR__, "wrappers", replace(triplet(p), "arm-" => "armv7l-") * ".jl")), platforms)
 
 # From the available options, choose the best platform
 best_platform = select_platform(Dict(p => triplet(p) for p in platforms))
