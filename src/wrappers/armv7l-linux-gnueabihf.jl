23fab78a349058e7577ee0fc0e6ac94206dd8e4e
jlbuild <juliabuildbot@gmail.com>
2020-08-26 18:37:39 +0000

    Xorg_libX11_jll build 1.6.9+3

--- src/wrappers/armv7l-linux-gnueabihf.jl
+++ src/wrappers/armv7l-linux-gnueabihf.jl
@@ -7,6 +7,7 @@ using Xorg_xtrans_jll
 PATH = ""
 LIBPATH = ""
 LIBPATH_env = "LD_LIBRARY_PATH"
+LIBPATH_default = ""
 
 # Relative path to `libX11`
 const libX11_splitpath = ["lib", "libX11.so"]
@@ -44,8 +45,6 @@ function __init__()
 
     # Initialize PATH and LIBPATH environment variable listings
     global PATH_list, LIBPATH_list
-    # We first need to add to LIBPATH_list the libraries provided by Julia
-    append!(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)])
     # From the list of our dependencies, generate a tuple of all the PATH and LIBPATH lists,
     # then append them to our own.
     foreach(p -> append!(PATH_list, p), (Xorg_libxcb_jll.PATH_list, Xorg_xtrans_jll.PATH_list,))
@@ -69,12 +68,8 @@ function __init__()
     filter!(!isempty, unique!(PATH_list))
     filter!(!isempty, unique!(LIBPATH_list))
     global PATH = join(PATH_list, ':')
-    global LIBPATH = join(LIBPATH_list, ':')
+    global LIBPATH = join(vcat(LIBPATH_list, [joinpath(Sys.BINDIR, Base.LIBDIR, "julia"), joinpath(Sys.BINDIR, Base.LIBDIR)]), ':')
 
-    # Add each element of LIBPATH to our DL_LOAD_PATH (necessary on platforms
-    # that don't honor our "already opened" trick)
-    #for lp in LIBPATH_list
-    #    push!(DL_LOAD_PATH, lp)
-    #end
+    
 end  # __init__()
 
