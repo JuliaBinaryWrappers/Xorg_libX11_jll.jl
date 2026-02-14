1c3dda27bb08892a9e19e9cd3da442f6ae484c9e
jlbuild <juliabuildbot@gmail.com>
2026-02-14 19:37:40 +0000

    Xorg_libX11_jll build 1.8.13+0

--- src/Xorg_libX11_jll.jl
+++ src/Xorg_libX11_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("Xorg_libX11")
-JLLWrappers.@generate_main_file("Xorg_libX11", UUID("4f6342f7-b3d2-589e-9d20-edeb45f2b2bc"))
+JLLWrappers.@generate_main_file("Xorg_libX11", Base.UUID("4f6342f7-b3d2-589e-9d20-edeb45f2b2bc"))
 end  # module Xorg_libX11_jll
